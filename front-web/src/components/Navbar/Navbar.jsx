a5530df563c22fe720c8ff874eaa23d33d11f16d
FernandoNv <fernandonvieira47@hotmail.com>
2021-01-06 11:47:32 -0300

    Added Navbar

---
+++ front-web/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import './Navbar.css';
+import { ReactComponent as Logo } from './logo.svg';
+function Navbar(){
+    return(
+        <nav className="main-navbar">
+            <Logo />
+            <a className="logo-text" href="#">DS Delivery</a>
+        </nav>
+    )
+}
+
+export default Navbar;
